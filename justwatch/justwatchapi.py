51783f530505a26b604d85d7b30cb2b13f6cdf97
dawoud_t <dtabbouh@gmail.com>
2018-12-04 21:50:56 +1100

    wrote some more tests. refactored class slightly

--- justwatch/justwatchapi.py
+++ justwatch/justwatchapi.py
@@ -3,194 +3,195 @@ from datetime import timedelta
 import requests
 from babel import Locale
 
-HEADER = {'User-Agent':'JustWatch Python client (github.com/dawoudt/JustWatchAPI)'}
+
+HEADER = {'User-Agent': 'JustWatch Python client (github.com/dawoudt/JustWatchAPI)'}
+
 
 class JustWatch:
-	def __init__(self, country='AU', use_sessions=True, **kwargs):
-		self.kwargs = kwargs
-		self.country = country
-		self.language = Locale.parse('und_{}'.format(self.country)).language
-		self.locale = self.language + '_' + self.country
-		self.kwargs_cinema = []
-		if use_sessions:
-			self.requests = requests.Session()
-		else:
-			self.requests = requests
-		
-	def search_for_item(self, **kwargs):
-		if kwargs:
-			self.kwargs = kwargs
-		null = None
-		payload = {
-			"age_certifications":null,
-			"content_types":null,
-			"presentation_types":null,
-			"providers":null,
-			"genres":null,
-			"languages":null,
-			"release_year_from":null,
-			"release_year_until":null,
-			"monetization_types":null,
-			"min_price":null,
-			"max_price":null,
-			"nationwide_cinema_releases_only":null,
-			"scoring_filter_types":null,
-			"cinema_release":null,
-			"query":null,
-			"page":null,
-			"page_size":null,
-			"timeline_type":null
-		}
-		for key, value in self.kwargs.items():
-			if key in payload.keys():
-				payload[key] = value
-			else:
-				print('{} is not a valid keyword'.format(key))
-		header = HEADER
-		api_url = 'https://api.justwatch.com/titles/{}/popular'.format(self.locale)
-		r = self.requests.post(api_url, json=payload, headers=header)
+    api_base = 'https://api.justwatch.com'
 
-		# Client should deal with rate-limiting. JustWatch may send a 429 Too Many Requests response.
-		r.raise_for_status()   # Raises requests.exceptions.HTTPError if r.status_code != 200
+    def __init__(self, country='AU', use_sessions=True, **kwargs):
+        self.kwargs = kwargs
+        self.country = country
+        self.language = Locale.parse('und_{}'.format(self.country)).language
+        self.locale = self.language + '_' + self.country
+        self.api_url_popular = '{api_base}/titles/{locale}/popular'.format(api_base=self.api_base,
+                                                                           locale=self.locale)
+        self.api_url_content = '{api_base}/content/'.format(api_base=self.api_base)
 
-		return r.json()
+        self.kwargs_cinema = []
+        self.requests = requests.Session() if use_sessions else requests
 
-	def get_providers(self):
+    def search_for_item(self, **kwargs):
+        if kwargs:
+            self.kwargs = kwargs
+        null = None
+        payload = {
+            "age_certifications": null,
+            "content_types": null,
+            "presentation_types": null,
+            "providers": null,
+            "genres": null,
+            "languages": null,
+            "release_year_from": null,
+            "release_year_until": null,
+            "monetization_types": null,
+            "min_price": null,
+            "max_price": null,
+            "nationwide_cinema_releases_only": null,
+            "scoring_filter_types": null,
+            "cinema_release": null,
+            "query": null,
+            "page": null,
+            "page_size": null,
+            "timeline_type": null
+        }
+        for key, value in self.kwargs.items():
+            if key in payload.keys():
+                payload[key] = value
+            else:
+                print('{} is not a valid keyword'.format(key))
+        r = self.requests.post(self.api_url_popular, json=payload, headers=HEADER)
 
-		header = HEADER
-		api_url = 'https://apis.justwatch.com/content/providers/locale/{}'.format(self.locale)
-		r = self.requests.get(api_url, headers=header)
+        # Client should deal with rate-limiting. JustWatch may send a 429 Too Many Requests response.
+        r.raise_for_status()   # Raises requests.exceptions.HTTPError if r.status_code != 200
 
-		# Client should deal with rate-limiting. JustWatch may send a 429 Too Many Requests response.
-		r.raise_for_status()   # Raises requests.exceptions.HTTPError if r.status_code != 200
+        return r.json()
 
-		return r.json()
-        
-	def get_genres(self):
+    def get_providers(self):
+        providers = '{api_url_content}/providers/locale/{locale}'.format(api_url_content=self.api_url_content,
+                                                                         locale=self.locale)
+        r = self.requests.get(providers, headers=HEADER)
 
-		header = HEADER
-		api_url = 'https://apis.justwatch.com/content/genres/locale/{}'.format(self.locale)
-		r = self.requests.get(api_url, headers=header)
+        # Client should deal with rate-limiting. JustWatch may send a 429 Too Many Requests response.
+        r.raise_for_status()   # Raises requests.exceptions.HTTPError if r.status_code != 200
+        return r.json()
 
-		# Client should deal with rate-limiting. JustWatch may send a 429 Too Many Requests response.
-		r.raise_for_status()   # Raises requests.exceptions.HTTPError if r.status_code != 200
+    def get_genres(self):
+        genres = '{api_url_content}/genres/locale/{locale}'.format(api_url_content=self.api_url_content,
+                                                                   locale=self.locale)
+        r = self.requests.get(genres, headers=HEADER)
 
-		return r.json()
+        # Client should deal with rate-limiting. JustWatch may send a 429 Too Many Requests response.
+        r.raise_for_status()   # Raises requests.exceptions.HTTPError if r.status_code != 200
+        return r.json()
 
-	def get_title(self, title_id, content_type='movie'):
+    def get_title(self, title_id, content_type='movie'):
+        titles = '{api_url_content}/titles/{content_type}/{title_id}/locale/{locale}'
+        titles = titles.format(api_url_content=self.api_url_content,
+                               content_type=content_type,
+                               title_id=title_id,
+                               locale=self.locale)
 
-		header = HEADER
-		api_url = 'https://apis.justwatch.com/content/titles/{}/{}/locale/{}'.format(content_type, title_id, self.locale)
-		r = self.requests.get(api_url, headers=header)
+        r = self.requests.get(titles, headers=HEADER)
 
-		# Client should deal with rate-limiting. JustWatch may send a 429 Too Many Requests response.
-		r.raise_for_status()   # Raises requests.exceptions.HTTPError if r.status_code != 200
+        # Client should deal with rate-limiting. JustWatch may send a 429 Too Many Requests response.
+        r.raise_for_status()   # Raises requests.exceptions.HTTPError if r.status_code != 200
+        return r.json()
 
-		return r.json()
+    def get_season(self, season_id):
 
-        
-	def get_season(self, season_id):
+        header = HEADER
+        api_url = 'https://apis.justwatch.com/content/titles/show_season/{}/locale/{}'.format(season_id, self.locale)
+        r = self.requests.get(api_url, headers=header)
 
-		header = HEADER
-		api_url = 'https://apis.justwatch.com/content/titles/show_season/{}/locale/{}'.format(season_id, self.locale)
-		r = self.requests.get(api_url, headers=header)
+        # Client should deal with rate-limiting. JustWatch may send a 429 Too Many Requests response.
+        r.raise_for_status()   # Raises requests.exceptions.HTTPError if r.status_code != 200
 
-		# Client should deal with rate-limiting. JustWatch may send a 429 Too Many Requests response.
-		r.raise_for_status()   # Raises requests.exceptions.HTTPError if r.status_code != 200
-
-		return r.json()
+        return r.json()
 
 
-	def get_cinema_times(self, title_id, content_type = 'movie', **kwargs):
+    def get_cinema_times(self, title_id, content_type = 'movie', **kwargs):
 
-		if kwargs:
-			self.kwargs_cinema = kwargs
+        if kwargs:
+            self.kwargs_cinema = kwargs
 
-		null = None
-		payload = {
-			"date":null,
-			"latitude":null,
-			"longitude":null,
-			"radius":20000
-		}
-		for key, value in self.kwargs_cinema.items():
-			if key in payload.keys():
-				payload[key] = value
-			else:
-				print('{} is not a valid keyword'.format(key))
-
-
-		header = HEADER
-		api_url = 'https://apis.justwatch.com/content/titles/{}/{}/showtimes'.format(content_type, title_id)
-		r = self.requests.get(api_url, params=payload, headers=header)
-
-		r.raise_for_status()   # Raises requests.exceptions.HTTPError if r.status_code != 200
-
-		return r.json()
+        null = None
+        payload = {
+            "date":null,
+            "latitude":null,
+            "longitude":null,
+            "radius":20000
+        }
+        for key, value in self.kwargs_cinema.items():
+            if key in payload.keys():
+                payload[key] = value
+            else:
+                print('{} is not a valid keyword'.format(key))
 
 
-	def get_cinema_details(self, **kwargs):
 
-		if kwargs:
-			self.kwargs_cinema = kwargs
+        header = HEADER
+        api_url = 'https://apis.justwatch.com/content/titles/{}/{}/showtimes'.format(content_type, title_id)
+        r = self.requests.get(api_url, params=payload, headers=header)
 
-		null = None
-		payload = {
-			"latitude":null,
-			"longitude":null,
-			"radius":20000
-		}
-		for key, value in self.kwargs_cinema.items():
-			if key in payload.keys():
-				payload[key] = value
-			elif key == 'date':
+        r.raise_for_status()   # Raises requests.exceptions.HTTPError if r.status_code != 200
+
+        return r.json()
+
+
+    def get_cinema_details(self, **kwargs):
+
+        if kwargs:
+            self.kwargs_cinema = kwargs
+
+        null = None
+        payload = {
+            "latitude":null,
+            "longitude":null,
+            "radius":20000
+        }
+        for key, value in self.kwargs_cinema.items():
+            if key in payload.keys():
+                payload[key] = value
+            elif key == 'date':
                 #ignore the date value if passed
-				pass
-			else:
-				print('{} is not a valid keyword'.format(key))
+                pass
+            else:
+                print('{} is not a valid keyword'.format(key))
 
 
-		header = HEADER
-		api_url = 'https://apis.justwatch.com/content/cinemas/{}'.format(self.locale)
-		r = self.requests.get(api_url, params=payload, headers=header)
+        header = HEADER
+        api_url = 'https://apis.justwatch.com/content/cinemas/{}'.format(self.locale)
+        r = self.requests.get(api_url, params=payload, headers=header)
 
-		r.raise_for_status()   # Raises requests.exceptions.HTTPError if r.status_code != 200
+        r.raise_for_status()   # Raises requests.exceptions.HTTPError if r.status_code != 200
 
-		return r.json()
+        return r.json()
 
 
 
-	def get_upcoming_cinema(self, weeks_offset, nationwide_cinema_releases_only=True):
+    def get_upcoming_cinema(self, weeks_offset, nationwide_cinema_releases_only=True):
 
-		header = HEADER
-		payload = { 'nationwide_cinema_releases_only': nationwide_cinema_releases_only, 
-						'body': {} }
-		now_date = datetime.now()
-		td = timedelta(weeks=weeks_offset)
-		year_month_day = (now_date + td).isocalendar()
-		api_url = 'https://apis.justwatch.com/content/titles/movie/upcoming/{}/{}/locale/{}'
-		api_url = api_url.format(year_month_day[0], year_month_day[1], self.locale)
+        header = HEADER
+        payload = { 'nationwide_cinema_releases_only': nationwide_cinema_releases_only, 
+                        'body': {} }
+        now_date = datetime.now()
+        td = timedelta(weeks=weeks_offset)
+        year_month_day = (now_date + td).isocalendar()
+        api_url = 'https://apis.justwatch.com/content/titles/movie/upcoming/{}/{}/locale/{}'
+        api_url = api_url.format(year_month_day[0], year_month_day[1], self.locale)
 
-		#this throws an error if you go too many weeks forward, so return a blank payload if we hit an error
-		try:        
-			r = self.requests.get(api_url, params=payload, headers=header)
+        #this throws an error if you go too many weeks forward, so return a blank payload if we hit an error
+        try:        
+            r = self.requests.get(api_url, params=payload, headers=header)
 
-			# Client should deal with rate-limiting. JustWatch may send a 429 Too Many Requests response.
-			r.raise_for_status()   # Raises requests.exceptions.HTTPError if r.status_code != 200
+            # Client should deal with rate-limiting. JustWatch may send a 429 Too Many Requests response.
+            r.raise_for_status()   # Raises requests.exceptions.HTTPError if r.status_code != 200
 
-			return r.json()
-		except:
-			return {'page': 0, 'page_size': 0, 'total_pages': 1, 'total_results': 0,  'items': []}
+            return r.json()
+        except:
+            return {'page': 0, 'page_size': 0, 'total_pages': 1, 'total_results': 0,  'items': []}
 
-	def get_certifications(self, content_type = 'movie'):
+    def get_certifications(self, content_type = 'movie'):
 
-		header = HEADER
-		payload = { 'country': self.country, 'object_type': content_type }
-		api_url = 'https://apis.justwatch.com/content/age_certifications'
-		r = self.requests.get(api_url, params=payload, headers=header)
+        header = HEADER
+        payload = { 'country': self.country, 'object_type': content_type }
+        api_url = 'https://apis.justwatch.com/content/age_certifications'
+        r = self.requests.get(api_url, params=payload, headers=header)
 
-		# Client should deal with rate-limiting. JustWatch may send a 429 Too Many Requests response.
-		r.raise_for_status()   # Raises requests.exceptions.HTTPError if r.status_code != 200
+        # Client should deal with rate-limiting. JustWatch may send a 429 Too Many Requests response.
+        r.raise_for_status()   # Raises requests.exceptions.HTTPError if r.status_code != 200
 
-		return r.json()
+        return r.json()
 
--- tests.py
+++ tests.py
@@ -1,9 +1,20 @@
 import unittest
 from justwatch import justwatchapi, JustWatch
+from requests import Session
 from requests.exceptions import HTTPError
+import sys
 
 
 class TestJustWatchAPI(unittest.TestCase):
+    def setUp(self):
+        self.just_watch = JustWatch(country='US')
+
+    def tearDown(self):
+        self.just_watch.requests.close()
+
+    def test_using_session(self):
+        self.assertIsInstance(self.just_watch.requests, Session)
+
     def test_header(self):
         ''' Assert header has not changed'''
 
@@ -14,9 +25,8 @@ class TestJustWatchAPI(unittest.TestCase):
     def test_get(self):
         '''Test that we at least don't fail to get a response'''
 
-        just_watch = JustWatch(country='US')
         try:
-            just_watch.search_for_item(query='the matrix', page_size=1)
+            self.just_watch.search_for_item(query='the matrix', page_size=1)
         except Exception as e:
             self.fail(f"test_get() failed with Exception: {e}")  # noqa
 
@@ -26,10 +36,42 @@ class TestJustWatchAPI(unittest.TestCase):
         search_item = 'the matrix'
 
         just_watch = JustWatch(country='US')
-        results = just_watch.search_for_item(query=search_item, page_size=1)
+        results = self.just_watch.search_for_item(query=search_item, page_size=1)
         first_result_title = results['items'][0]['title'].lower()
 
         self.assertEqual(search_item, first_result_title)
+
+    def test_get_providers(self):
+        ''' Test we actually get providers '''
+
+        try:
+            self.just_watch.get_providers()
+        except Exception as e:
+            self.fail(f"test_get_providers() failed with Exception: {e}")  # noqa
+
+    def test_providers_has_netflix(self):
+        ''' Test we get netflix as a provider '''
+
+        res = self.just_watch.get_providers()
+        has_netlix = any('netflix' == prov['technical_name'] for prov in res)
+        self.assertTrue(has_netlix)
+
+    def test_get_genres(self):
+        ''' Test we get some genres back'''
+
+        try:
+            self.just_watch.get_genres()
+        except Exception as e:
+            self.fail(f"test_get_genres() failed with Exception: {e}")  # noqa
+
+    def test_get_title(self):
+        ''' Test we get some genres back'''
+        the_matrix_title_id = 10
+
+        try:
+            self.just_watch.get_title(the_matrix_title_id)
+        except Exception as e:
+            self.fail(f"test_get_title() failed with Exception: {e}")  # noqa
 
 
 if __name__ == '__main__':
